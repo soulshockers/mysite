66d9d3023ed0e2eed37be7e6cbfe9d9a7957da82
Vasyl Sovyak <vasyl.sovyak@paysera.net>
2025-05-11 21:19:40 +0300

    Update dependencies and configure production settings
    This commit updates `requirements.txt` with all dependencies, modifies the `render.yaml` to use WSGI and set necessary environment variables, and configures `production.py` for deployment. Changes include database configuration, static file handling with WhiteNoise, and dynamically setting allowed hosts.

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,39 @@
+import dj_database_url
+
 from .base import *
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+
 DEBUG = False
+
+# https://docs.djangoproject.com/en/5.2/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+MIDDLEWARE.insert(
+    MIDDLEWARE.index('django.middleware.security.SecurityMiddleware') + 1,
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+)
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
+
+# Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+# and renames the files with unique names for each version to support long-term caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 try:
     from .local import *
